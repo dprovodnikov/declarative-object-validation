e1a5caf653cb959a52c64c82447a67ac7bba8c1b
dprovodnikov <dp.wireden@gmail.com>
2019-06-02 20:30:54 +0300

    Throw an error if validation key is not specified

--- src/applyRules.js
+++ src/applyRules.js
@@ -2,6 +2,10 @@ import validate from './validate';
 import validateAll from './validateAll';
 
 const applyRules = rules => (fieldName, object, options = {}) => {
+  if (!fieldName) {
+    throw Error('Missing validation key. What do you want to validate?');
+  }
+
   if (fieldName === 'all') {
     return validateAll(rules, options.omit, field => ({
       [field]: validate(rules[field], object[field], object),
