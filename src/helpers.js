bf8beb9adf5263f65b43b8188d48ed5655f5d35e
dprovodnikov <dp.wireden@gmail.com>
2019-06-04 19:58:34 +0300

    Add a "reset" helper function

--- src/helpers.js
+++ src/helpers.js
@@ -5,3 +5,7 @@ export const hasError = (validationReport) => {
 export const getFirstError = (validationReport) => {
   return Object.values(validationReport).filter(value => !!value).shift();
 };
+
+export const reset = validationReport => Object.entries(validationReport)
+  .map(entry => [entry[0], ''])
+  .reduce((output, entry) => ({ ...output, [entry[0]]: entry[1] }), {});
